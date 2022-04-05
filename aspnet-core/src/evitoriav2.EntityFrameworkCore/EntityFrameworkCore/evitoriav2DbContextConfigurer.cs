6e64140222bbab51ae4fa816b78d22959221aced
ntgentil <impulso.antonio@parceiro.elevaeducacao.com.br>
2022-04-05 17:49:59 -0300

    implementação utilizando mysql

--- aspnet-core/src/evitoriav2.EntityFrameworkCore/EntityFrameworkCore/evitoriav2DbContextConfigurer.cs
+++ aspnet-core/src/evitoriav2.EntityFrameworkCore/EntityFrameworkCore/evitoriav2DbContextConfigurer.cs
@@ -9,13 +9,13 @@ namespace evitoriav2.EntityFrameworkCore
         public static void Configure(DbContextOptionsBuilder<evitoriav2DbContext> builder, string connectionString)
         {
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
-            builder.UseMySql(connectionString, serverVersion, options => options.EnableRetryOnFailure());
+            builder.UseMySql(connectionString, serverVersion);
         }
 
         public static void Configure(DbContextOptionsBuilder<evitoriav2DbContext> builder, DbConnection connection)
         {
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
-            builder.UseMySql(connection, serverVersion, options => options.EnableRetryOnFailure());
+            builder.UseMySql(connection, serverVersion);
         }
     }
 }
